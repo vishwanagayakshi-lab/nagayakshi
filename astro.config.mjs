ff1a25e0f2f51e1d3bb51df00a7a9b97667e55ef
bodhispace <bodhi.space24@gmail.com>
2026-04-19 12:32:20 +0530

    Remove sitemap integration to fix Vercel build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://nagayakshi.org',
-  integrations: [sitemap()],
   output: 'static',
 });
